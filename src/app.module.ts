de4fa97a2575caeb2d2f608d8008b0a7995aaff7
Wesley <wesley.leoncio.izi@gmail.com>
2024-01-18 00:42:53 -0300

    Configurando o database do projeto.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CategoriaModule } from "./modules/categoria/categoria.module";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigService } from './config/database.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [CategoriaModule],
+  imports: [
+    CategoriaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+      inject: [DatabaseConfigService],
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/modules/categoria/models/entity/categoria.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'categorias' })
+export class CategoriaEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'nome', length: 100, nullable: false, unique: true })
+  nome: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+}
