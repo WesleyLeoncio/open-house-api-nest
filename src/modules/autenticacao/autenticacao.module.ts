2d7c741ff3c0eb83d3ea701df6640c92149a6028
Wesley <wesley.leoncio.izi@gmail.com>
2024-01-26 16:14:38 -0300

    Implementação do token jwt

--- src/modules/autenticacao/autenticacao.module.ts
+++ src/modules/autenticacao/autenticacao.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AutenticacaoController } from './controller/autenticacao.controller';
 import { AutenticacaoService } from './service/autenticacao.service';
 import { UsuarioModule } from '../usuario/usuario.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return{
+          secret: configService.get<string>('SECRET_JWT'),
+          signOptions: {expiresIn: '2h'}
+        }
+      },
+      inject: [ConfigService],
+      global: true
+    })
+  ],
   controllers: [AutenticacaoController],
   providers: [
     AutenticacaoService
--- src/modules/autenticacao/service/autenticacao.service.ts
+++ src/modules/autenticacao/service/autenticacao.service.ts
@@ -2,21 +2,27 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsuarioEntity } from '../../usuario/models/entity/usuario.entity';
 import { Bcrypt } from '../../security/bcrypt';
 import { UsuarioService } from '../../usuario/service/usuario.service';
+import { JwtService } from '@nestjs/jwt';
+import { Payload } from '../models/jwt/payload';
+import { Token } from '../models/jwt/token';
 
 @Injectable()
 export class AutenticacaoService {
 
-  constructor(private readonly usuarioService: UsuarioService){
+  constructor(
+    private readonly usuarioService: UsuarioService,
+    private jwtService: JwtService) {
   }
 
-  async login(login: string, senha: string){
+  async login(login: string, senha: string):Promise<Token> {
     const usuario: UsuarioEntity = await this.usuarioService.verificarLogin(login);
     const checkPassword: boolean = await Bcrypt.checkPassword(senha, usuario.senha);
     if (!checkPassword) throw new UnauthorizedException('Senha incorreta!');
-    return usuario;
+
+    const payload :Payload = { id: usuario.id, nome: usuario.nome, login: usuario.login }
+
+    return { token: await this.jwtService.signAsync(payload) };
   }
-
-
 
 
 }
