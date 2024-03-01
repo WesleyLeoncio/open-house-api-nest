29db776e679b76480872cdd5758af0895ee15db1
Wesley <wesley.leoncio.izi@gmail.com>
2024-03-01 11:57:40 -0300

    começando implentar swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationError, ValidationPipe } from '@nestjs/common';
 import { ValidationExeption } from './infra/exceptions/ValidationExeption';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,15 @@ async function bootstrap() {
 
     })
   );
+  const config = new DocumentBuilder()
+    .setTitle('OPEN HOUSE NESTJS')
+    .setDescription('API DE FILMES')
+    .setVersion('1.0')
+    .addTag('filmes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap().finally();
